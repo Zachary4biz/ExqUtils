1b6422baabfe6672f75e9157f60896c068bd8ac6
zhoutong <zhoutong@apusapps.com>
2019-05-28 13:45:08 +0800

    init python utils

--- python_utils/ExqUtils.py
+++ python_utils/ExqUtils.py
@@ -3,7 +3,7 @@ class ExqUtils(object):
         pass
 
     @staticmethod
-    def loadFileAsIter(path):
-        with open(path,"r+") as f:
+    def load_file_as_iter(path):
+        with open(path, "r+") as f:
             for i in f:
                 yield i
