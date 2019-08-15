a659a02321de39483493f7d45517a1725ea0619c
zhoutong <zhoutong@apusapps.com>
2019-08-15 13:01:45 +0800

    update

--- python_utils/verify.py
+++ python_utils/verify.py
@@ -9,13 +9,18 @@ if __name__ == '__main__':
     # 验证 groupby
     item = [(1, 2, 'a'), (1, 3, 'b'), (1, 4, 'c'), (2, 2, 'd'), (2, 3, 'e'), (2, 4, 'f')]
     random.shuffle(item)
+    item2 = [1, 2, 3, 4, 5, 1, 1, 1, 6, 2, 2, 2, 7, 7, 7, 5, 4, 4, 3, 6, 8, 8]
+
+    zprint("ExqUtils default:")
+    for k, g in ExqUtils.groupby(item2):
+        print(k, list(g))
     zprint("ExqUtils:")
     for k, g in ExqUtils.groupby(item, key=lambda x: x[1]):
         print(k, list(g))
     zprint("itertools:")
     for k, g in itertools.groupby(item, key=lambda x: x[1]):
         print(k, list(g))
-
+    assert False
     # 验证 map_on_iter 方法
     item = range(0, 5000, 7)
     print("iterItem is ", list(item))
