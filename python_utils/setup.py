d9d6eda0a537e5791ee7d2b3bd95626f2d121865
zhoutong <zhoutong@apusapps.com>
2019-08-16 12:13:49 +0800

    update

--- python_utils/setup.py
+++ python_utils/setup.py
@@ -11,7 +11,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='zac_pyutils',
-    version='1.55',
+    version='1.56',
     description=(
         'collection of some useful functions'
     ),
