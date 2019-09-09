d4e0e7d55fcc48ecf647d04f1ee3da7bd68ac425
zhoutong <zhoutong@apusapps.com>
2019-09-09 16:38:01 +0800

    update: 支持多图片 取消了部分print

--- python_utils/zac_pyutils/demo/WriteExcel/WriteImg2Excel.py
+++ python_utils/zac_pyutils/demo/WriteExcel/WriteImg2Excel.py
@@ -5,6 +5,7 @@ import xlsxwriter as xw
 import os
 from io import BytesIO
 from PIL import Image
+from tqdm.auto import tqdm
 import datetime
 
 
@@ -22,7 +23,7 @@ with open("./config.txt", "r") as fr:
     content = {i[0]: "=".join(i[1:]) for i in content}
 
 # fp = "/Users/zac/Downloads/tiles_demo.xlsx"
-book_fp = "./demo.xlsx"
+book_fp = "./{}".format(content['outputFileName'])
 logo_left = Image.open("./logo_left.png")
 logo_right = Image.open("./logo_right.png")
 pic_dir = content['picsDir']
@@ -32,7 +33,7 @@ pic_fp_list = [os.path.join(pic_dir, i) for i in os.listdir(pic_dir) if os.path.
 wb = xw.Workbook(book_fp)
 ws1 = wb.add_worksheet("abc")
 cell_w, cell_h = ws1.default_col_pixels, ws1.default_row_pixels
-print(cell_w, cell_h)
+# print(cell_w, cell_h)
 logo_left = logo_left.resize((int(cell_w*(0.6+0.6)), int(cell_h*(0.5+2+0.5))))
 logo_right = logo_right.resize((int(cell_w*(0.8+2+0.8)), int(cell_h*(0.5+2+0.5))))
 #########
@@ -106,7 +107,7 @@ anchor_left_bottom_format = wb.add_format({"left":2,"bottom":2})
 ############
 # 插入logo
 ############
-print(logo_left.size, logo_right.size)
+# print(logo_left.size, logo_right.size)
 ws1.insert_image("A1", "left", options={'image_data': get_image_as_bytes(logo_left), 'x_offset' : int(cell_w*0.4), 'y_offset': int(cell_h*0.5)})
 ws1.insert_image("O1", "right", options={'image_data': get_image_as_bytes(logo_right), 'x_offset': int(cell_w*0.2), 'y_offset': int(cell_h*0.5)})
 for i in [0, 1, 14, 15, 16, 17]:
@@ -140,36 +141,40 @@ ws1.merge_range("N10:R11", date.strip(), generic_format)
 # 插入图片
 ################
 cnt = 0
-for fp in pic_fp_list:
+reservedRowsFrom = 0  # 最后一行后面要留出10个空行
+for fp in tqdm(pic_fp_list):
     f_name = os.path.splitext(os.path.basename(fp))[0]
     img = Image.open(fp)
     # 图片：宽7个cell，高 16 个cell
     img = img.resize((int(cell_w*7), int(cell_h*16)))
     # 图片的边距：距离左边1个cell，距离上面 0个cell
-    img_row, img_col = 12+cnt//2*(16+2), 0+cnt%2*9
-    print("cnt-{} 图片坐标:".format(cnt), (img_row, img_col))
+    img_row, img_col = 12+cnt//2*(16+2), 1+cnt % 2*9
+    # print("cnt-{} 图片坐标:".format(cnt), (img_row, img_col))
     ws1.insert_image(row=img_row, col=img_col, filename=f_name,
-                     options={'image_data': get_image_as_bytes(img), 'x_offset': int(cell_w * 1)})
+                     options={'image_data': get_image_as_bytes(img)})
     # 图片的名字填在这里：注意这里下标是以0开始的 12 是图片放置的坐标(这里是0开始的，如果excel里看是第13行)
-    first_row, first_col = 12 + 16 + cnt // 2 * (16 + 2), 0 + cnt % 2 * 9
-    last_row, last_col = first_row + 2 - 1, first_col + 9 - 1  # 右下角的坐标相比左上角，高2宽9，为了得到坐标要减去1
-    print("cnt-{} 文字坐标:".format(cnt), (first_row, first_col), (last_row, last_col))
+    first_row, first_col = 12 + 16 + cnt // 2 * (16 + 2), 1 + cnt % 2 * 9
+    last_row, last_col = first_row + 2 - 1, first_col + 7 - 1  # 右下角的坐标相比左上角，高2宽7，为了得到坐标要减去1
+    # print("cnt-{} 文字坐标:".format(cnt), (first_row, first_col), (last_row, last_col))
     ws1.merge_range(first_row, first_col, last_row, last_col, data=f_name.strip(), cell_format=picName_format)
+    reservedRowsFrom = last_row + 10
     cnt += 1
+# 75是6张图的情况
+reservedRowsFrom = 75 if reservedRowsFrom <= 75 else reservedRowsFrom
 ################
 # 构造加厚的边框
 ################
 # 竖着左边
-for i in range(11,75+1):
-    ws1.write(i,0,"",left_border_format)
+for i in range(11, reservedRowsFrom + 1):
+    ws1.write(i, 0, "", left_border_format)
 # 竖着右边
-for i in range(11,75+1):
-    ws1.write(i,17,"",right_border_format)
+for i in range(11, reservedRowsFrom + 1):
+    ws1.write(i, 17, "", right_border_format)
 # 横着下边
-for i in range(0,17+1):
-    ws1.write(75,i,"",bottom_border_format)
+for i in range(0, 17 + 1):
+    ws1.write(reservedRowsFrom, i, "", bottom_border_format)
 # 连接处要改一下
-ws1.write(75,17,"",anchor_right_bottom_format) #右下角
-ws1.write(75,0,"",anchor_left_bottom_format) #左下角
+ws1.write(reservedRowsFrom, 17, "", anchor_right_bottom_format)  # 右下角
+ws1.write(reservedRowsFrom, 0, "", anchor_left_bottom_format)  # 左下角
 
 wb.close()
